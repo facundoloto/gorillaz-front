2e308628853d6b989d6d2746f011c9b4acfbe28b
ema <lotofacundo6@gmail.com>
2022-12-26 01:17:28 -0300

    add new url

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Member from '../components/Member/Member.jsx';
 import ExMember from '../components/ExMember/ExMember.jsx';
 import Disks from '../components/Disks/Disks.jsx';
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path='/' element={<Member />} />
-      <Route path='/exmember' element={<ExMember />} />
-      <Route path='/disks' element={<Disks />} /> 
-      {/* <Route path='/' element={<Doc />} /> */}
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Member />} />
+        <Route path='/exmember' element={<ExMember />} />
+        <Route path='/disks' element={<Disks />} />
+        {/* <Route path='/' element={<Doc />} /> */}
+      </Routes>
+    </BrowserRouter>
+
   );
 }
