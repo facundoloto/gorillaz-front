62c1f6882c952d1afecc72489c206cca15a6d615
ema <lotofacundo6@gmail.com>
2023-09-14 16:26:14 -0300

    fix router

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Member from '../components/Member/Member.jsx';
 import ExMember from '../components/ExMember/ExMember.jsx';
 import Disks from '../components/Disks/Disks.jsx';
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-
+    <Router>
       <Routes>
         <Route path='/' element={<Member />} />
         <Route path='/exmember' element={<ExMember />} />
         <Route path='/disk' element={<Disks />} />
         {/* <Route path='/' element={<Doc />} /> */}
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
